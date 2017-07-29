475276dd07f79c7ed155548d3a7a792f04129b31
StephenJackson <sejacks2@uno.edu>
2017-07-29 14:23:20 -0700

    added embeded youtube videos to reiew details

--- src/app/review-details/review-details.component.ts
+++ src/app/review-details/review-details.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ReviewService } from '../review-service/review.service';
 import { Review } from '../review-service/review';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
 
 @Component({
     selector: 'review-details',
@@ -14,11 +16,30 @@ export class ReviewDetailsComponent {
     private reviews: Review[];
     private review: Review;
     private error: string;
+    private url: SafeResourceUrl;
+    private sanitizer: DomSanitizer;
 
 
-    constructor(route: ActivatedRoute, reviewService: ReviewService) {
+    constructor(route: ActivatedRoute, reviewService: ReviewService, sanitizer: DomSanitizer) {
+        this.sanitizer = sanitizer;
+
         route.params.subscribe(params => this.reviewId = params['reviewId']);
-        reviewService.getReviewById(this.reviewId).then((review) => this.review = review);
+        reviewService.getReviewById(this.reviewId).then((review) => {this.review = review
 
+        console.log(this.review.link);
+
+        this.mapUrl(this.review.link);
+        console.log(this.url);
+        });
+
+    }
+
+    mapUrl(link: string){
+        var url;
+        url =  "https://www.youtube.com/embed/";
+        url = url.concat(link)
+
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+              
     }
 }
